93c705619bb2a89e66984498f00d13e3c49a3fb9
Anton Bezborody <anton.bezborody@gmail.com>
2023-07-17 16:08:45 +0400

    Disableable button toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,21 @@ console.log(fas);
 export class AppComponent {
   title = 'docusketch-test';
   index: number = Math.floor(Math.random() * Object.keys(fas).length);
-  icon = Object.values(fas)[this.index];
   buttonIsActive: boolean = true;
+  buttonIsDisableable: boolean = true;
+  icon = Object.values(fas)[this.index];
   loader = fas['faSpinner'];
+
   shuffle() {
-    // If we won't stack promises, we can just disable button and wait until timeout will change the icon.
-    // this.buttonIsActive = false;
+    if (this.buttonIsDisableable) this.buttonIsActive = false;
     setTimeout(() => {
       this.index = Math.floor(Math.random() * Object.keys(fas).length);
       this.icon = Object.values(fas)[this.index];
-      // After changing icon set button active back.
-      // this.buttonIsActive = true;
+      if (this.buttonIsDisableable) this.buttonIsActive = true;
     }, 3000);
+  }
+
+  disableButton() {
+    this.buttonIsDisableable = !this.buttonIsDisableable;
   }
 }
